11a71e3b6df346414e46148f45ac7e7705fdb3a6
jesuspb2 <64372399+jesuspb2@users.noreply.github.com>
2024-03-03 19:23:45 +0100

    Update bitcoin_price.py script
    - Added two lines with polyfit method
    - Added function to modify the axes formtat

--- bitcoin_price.py
+++ bitcoin_price.py
@@ -1,6 +1,8 @@
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.ticker import FuncFormatter
 
 
 def fetch_historical_data(coin_id='bitcoin', vs_currency='usd'):
@@ -32,6 +34,16 @@ def combine_data(df_api, df_csv):
     return df_combined
 
 
+def readable_formatter(value, pos):
+    if value >= 1e6:
+        val_str = '{:.0f}M'.format(value/1e6)
+    elif value >= 1e3:
+        val_str = '{:.0f}K'.format(value/1e3)
+    else:
+        val_str = str(int(value))
+    return val_str
+
+
 # Fetch data from the API
 df_api_data = fetch_historical_data()
 
@@ -45,14 +57,43 @@ df_combined_data = combine_data(df_api_data, df_csv_data)
 base_date = pd.to_datetime('2009-01-03')
 df_combined_data['days_since_base'] = (df_combined_data['date'] - base_date).dt.days
 
+x_log = np.log(df_combined_data['days_since_base'])
+y_log = np.log(df_combined_data['price'])
+
+slope, intercept = np.polyfit(x_log, y_log, 1)
+y_fit = np.exp(intercept) * df_combined_data['days_since_base']**slope
+
+new_intercept = intercept - 1
+y_fit_displaced = np.exp(new_intercept) * df_combined_data['days_since_base']**slope
+
+
 # Plotting
 plt.figure(figsize=(10, 5))
-plt.plot(df_combined_data['days_since_base'], df_combined_data['price'], label='Bitcoin Price', color='blue')
+plt.plot(df_combined_data['days_since_base'],
+         df_combined_data['price'],
+         label='Bitcoin Price',
+         color='blue',
+         alpha=0.5)
+plt.plot(df_combined_data['days_since_base'],
+         y_fit,
+         label='Fit Line',
+         color='red',
+         linestyle='--')
+plt.plot(df_combined_data['days_since_base'],
+         y_fit_displaced,
+         label='Fit Line Displaced',
+         color='green',
+         linestyle='--')
 plt.xscale('log')
 plt.yscale('log')
+desired_ticks = [1000, 2000, 3000, 4000, 5000, 6000]
+desired_tick_labels = [readable_formatter(i*1000, None) if i >= 1 else str(i) for i in desired_ticks]
+plt.xticks(desired_ticks, desired_tick_labels)
 plt.xlabel('Days Since Coinbase (2009-01-03)')
 plt.ylabel('Price (USD)')
 plt.title('Bitcoin Price (Log-Log Scale)')
+plt.gca().xaxis.set_major_formatter(FuncFormatter(readable_formatter))
+plt.gca().yaxis.set_major_formatter(FuncFormatter(readable_formatter))
 plt.legend()
 plt.grid(True, which="both", ls="--")
 plt.show()
